a28999b20dfbc70ec8f057228149641db7541c20
Adrian Daconte <daconteadrian2018@gmail.com>
2024-08-05 12:46:32 -0500

    popup logica

--- src/js/animaciones.js
+++ src/js/animaciones.js
@@ -1,1 +1,24 @@
+import { verificación } from "./app.js";
+
+export const popup = document.querySelector(".div-popup");
+
+export function cerrarPopUp() {
+  
+  const cerrar = document.querySelector(".cerrar-popup");
+  cerrar.addEventListener("click", () => {
+    popup.classList.remove("div-popup");
+    popup.classList.add("non");
+
+    console.log("verificacion es ", verificación);
+  });
+}
+
+export function abrirPopUp() {
+   
+   popup.classList.add('div-popup');
+   popup.classList.remove('non');
+    
+
+} 
+
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,42 @@
+import {cerrarPopUp , abrirPopUp , popup} from "./animaciones.js";
 
-let botonEncriptar = document.querySelector('#botonEncriptar');
-botonEncriptar.addEventListener('click', () => {
 
-    let textoArea = document.querySelector('#area__input').value;
-    textoArea = textoArea.split('');
+const btnEncriptar = document.querySelector("#botonEncriptar");
+const expresionRegular = /^[a-z\s]+$/;
+export let verificación;
 
-    console.log(textoArea.length);
+function revisarTexto() {
+  btnEncriptar.addEventListener("click", () => {
+    let textoRecibido = document.querySelector("#area__input").value;
+    let verificado = undefined;
 
-    
-    
-});
+    if (expresionRegular.test(textoRecibido)) {
+      verificado = true;
+      verificación = verificado;
+      abrirPopUp();
+
+    } else {
+      verificado = false;
+      verificación = verificado;
+    }
+
+    return verificación;
+  });
+}
+
+
+revisarTexto();
+cerrarPopUp();
+
+//funcion para encriptar el texto
+function encriptar() {
+  btnEncriptar.addEventListener("click", () => {
+    let textoRecibido = document.querySelector("#area__input").value;
+
+    textoRecibido = textoRecibido.split("");
+    console.log(textoRecibido);
+  });
+}
+encriptar();
+
+//funcion para  revisar condiciones del texto
